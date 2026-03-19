a28980eae7f28b4878b4f7fca8dafe85f00893b1
atakankizilyuce <atakankizilyuce@outlook.com>
2026-03-19 20:42:15 +0300

    added player and volume for linux

---
+++ internal/alarm/player_linux.go
@@ -0,0 +1,74 @@
+//go:build linux
+
+package alarm
+
+import (
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+var (
+	playerMu  sync.Mutex
+	playerCmd *exec.Cmd
+	tempFile  string
+)
+
+// playWAV writes WAV data to a temp file and plays it with paplay or aplay.
+// It blocks until playback finishes or stopCh is closed.
+func playWAV(wavData []byte, stopCh <-chan struct{}) error {
+	playerMu.Lock()
+	if tempFile == "" {
+		f, err := os.CreateTemp("", "leavesafe-alarm-*.wav")
+		if err != nil {
+			playerMu.Unlock()
+			return err
+		}
+		f.Write(wavData)
+		f.Close()
+		tempFile = f.Name()
+	}
+
+	// Try paplay (PulseAudio) first, fall back to aplay (ALSA).
+	var cmd *exec.Cmd
+	if _, err := exec.LookPath("paplay"); err == nil {
+		cmd = exec.Command("paplay", tempFile)
+	} else {
+		cmd = exec.Command("aplay", tempFile)
+	}
+	playerCmd = cmd
+	playerMu.Unlock()
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	done := make(chan error, 1)
+	go func() { done <- cmd.Wait() }()
+
+	select {
+	case err := <-done:
+		return err
+	case <-stopCh:
+		cmd.Process.Kill()
+		<-done
+		return nil
+	}
+}
+
+// stopPlayback kills any running audio process and cleans up.
+func stopPlayback() {
+	playerMu.Lock()
+	defer playerMu.Unlock()
+
+	if playerCmd != nil && playerCmd.Process != nil {
+		playerCmd.Process.Kill()
+		playerCmd = nil
+	}
+	if tempFile != "" {
+		os.Remove(tempFile)
+		tempFile = ""
+	}
+	time.Sleep(50 * time.Millisecond)
+}
